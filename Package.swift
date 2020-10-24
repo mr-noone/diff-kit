b9dd462e04ed5e963a2aa4e1f04e9a490643f8b1
Aleksey Zgurskiy <mr.noone@icloud.com>
2020-10-24 22:19:04 +0300

    Add apply method to collection view

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "DiffKit",
-  platforms: [.iOS(.v8)],
+  platforms: [.iOS(.v9)],
   products: [
     .library(name: "DiffKit", targets: ["DiffKit"])
   ],
